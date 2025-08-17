14d567e366ac28fc83206c2f8a4c044631c19d57
Jeff Mofor <allojeff444@gmail.com>
2025-08-16 20:23:53 -0400

    Make code run on python3.7

--- main/yolov/utils/dataloaders.py
+++ main/yolov/utils/dataloaders.py
@@ -774,7 +774,8 @@ class LoadImagesAndLabels(Dataset):
         index = self.indices[index]  # linear, shuffled, or image_weights
 
         hyp = self.hyp
-        if mosaic := self.mosaic and random.random() < hyp["mosaic"]:
+        mosaic = self.mosaic and random.random() < hyp["mosaic"]
+        if mosaic:
             # Load mosaic
             img, labels = self.load_mosaic(index)
             shapes = None
@@ -1161,7 +1162,8 @@ def verify_image_label(args):
                     segments = [np.array(x[1:], dtype=np.float32).reshape(-1, 2) for x in lb]  # (cls, xy1...)
                     lb = np.concatenate((classes.reshape(-1, 1), segments2boxes(segments)), 1)  # (cls, xywh)
                 lb = np.array(lb, dtype=np.float32)
-            if nl := len(lb):
+            nl = len(lb)
+            if nl:
                 assert lb.shape[1] == 5, f"labels require 5 columns, {lb.shape[1]} columns detected"
                 assert (lb >= 0).all(), f"negative label values {lb[lb < 0]}"
                 assert (lb[:, 1:] <= 1).all(), f"non-normalized or out of bounds coordinates {lb[:, 1:][lb[:, 1:] > 1]}"
