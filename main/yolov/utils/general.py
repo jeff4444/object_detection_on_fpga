0be31d53ca85815e3502d71edc11524924dd8962
Jeff Mofor <allojeff444@gmail.com>
2025-08-16 20:25:42 -0400

    Make code run on python3.7

--- main/yolov/utils/general.py
+++ main/yolov/utils/general.py
@@ -174,7 +174,8 @@ def user_config_dir(dir="Ultralytics", env_var="YOLOV5_CONFIG_DIR"):
     """Returns user configuration directory path, preferring environment variable `YOLOV5_CONFIG_DIR` if set, else OS-
     specific.
     """
-    if env := os.getenv(env_var):
+    env = os.getenv(env_var)
+    if env:
         path = Path(env)  # use environment variable
     else:
         cfg = {"Windows": "AppData/Roaming", "Linux": ".config", "Darwin": "Library/Application Support"}  # 3 OS dirs
